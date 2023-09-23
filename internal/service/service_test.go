dd3b9df4c931fde71a80b21e5f77682f52567b44
fajarcandraaa <fajarcandraaa@gmail.com>
2023-09-23 12:46:08 +0700

    fix database connection for unit testing

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,28 +1,30 @@
 package service_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/fajarcandraaa/shortened_url/internal/entity"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/require"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func testConfig(t *testing.T) (*gorm.DB, error) {
-	var (
-		dsn = "host=localhost user=postgres dbname=db_shortened_url_test sslmode=disable password=postgres port=5433"
-	)
-
-	db, err := gorm.Open("postgres", dsn) // initiate database for testing
+	err := godotenv.Load("../../.env") // Update the path accordingly
+	require.NoError(t, err)
+	DBDriver := os.Getenv("DB_DRIVER_TEST")
+	DBHost := os.Getenv("DB_HOST_TEST")
+	DBUser := os.Getenv("DB_USER_TEST")
+	DBPassword := os.Getenv("DB_PASSWORD_TEST")
+	DBName := os.Getenv("DB_NAME_TEST")
+	DBPort := os.Getenv("DB_PORT_TEST")
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DBHost, DBPort, DBUser, DBName, DBPassword)
+	db, err := gorm.Open(DBDriver, DBURL) // initiate database for testing
 	require.NoError(t, err)
 	db.AutoMigrate(&entity.Url{})
-
-	err = godotenv.Load("../../.env") // Update the path accordingly
-	if err != nil {
-		return nil, err
-	}
 
 	return db, nil
 
